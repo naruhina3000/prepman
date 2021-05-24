b9edaa73475b2247355a133d7ccff73414bd20c4
Shari Breitenstein-Werner <shari_breitenstein@yahoo.de>
2021-05-24 17:32:56 +0800

    basic routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,28 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: 'pages#home'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root to: 'recipes#index'
+  
+  resources :recipes do
+    resources :ingredients, only: [:index, :create, :update]
+    resources :utensils, only: [:index, :create, :update]
+    resources :steps, only: [:index, :new, :create, :edit, :update]
+    resources :reviews, only: [:create]
+    resources :planners, only: [:index, :create, :destroy]
+    resources :favorites, only: [:create]
+    resources :cooked_recipe, only: [:create]
+    resources :cookbook_recipe, only: [:create]
+  end
+
+  resources :shopping_list, only: [:index, :create, :show, :destroy] do
+    ressources :shopping_list_ingredients, only: [:create, :destroy]
+  end
+
+  resources :favorites, only: [:destroy]
+  resources :cooked_recipe, only: [:destroy]
+  resources :cookbook_recipe, only: [:destroy]
+
+  get "/users/:id", to: "users#show", as: :user
+  get "/users/:id/edit", to: "users#edit", as: :user_edit
+  patch "/users/:id", to: "users#update"
+  
 end
