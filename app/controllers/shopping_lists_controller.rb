9f444d8c05a26bdfe8fdd19cb3f6efcaaece31ea
Jacqueline <j.sutjiawan@gmail.com>
2021-05-25 19:15:06 +0800

    added shopping list controller

--- app/controllers/shopping_lists_controller.rb
+++ app/controllers/shopping_lists_controller.rb
@@ -1,41 +1,45 @@
 class ShoppingListsController < ApplicationController
-  before_action :set_shopping_list, only: [:show, :edit, :update, :destroy]
+  before_action :set_shopping_list, only: [:show, :destroy]
 
 
-  resources :shopping_list, only: [:index, :create, :show, :destroy] do
-    resources :shopping_list_ingredients, only: [:create, :destroy]
-  end
+  # resources :shopping_list, only: [:index, :create, :show, :destroy] do
+  #   resources :shopping_list_ingredients, only: [:create, :destroy]
+  # end
 
 
 
   def index
     @shopping_lists = current_user.shopping_lists
+    @shopping_list = ShoppingList.new
+  end
+
+  def new
+    @shopping_list = ShoppingList.new
+  end
+
+  def show
+    @shopping_lists = current_user.shopping_lists
   end
 
 
-
 def create
-    @shopping_list = ShoppingList.new(planner_params)
-    @recipe = Recipe.find(params[:planner][:recipe_id])
-    @user = current_user
-    @planner.recipe = @recipe
-    @planner.user = @user
-    if @planner.save
-      redirect_to @recipe
+    @shopping_list = ShoppingList.new(shopping_list_params)
+    @shopping_list.user = current_user
+    if @shopping_list.save
+      redirect_to @shopping_list
     else
-      render "/recipes/show"
+      render "/"
     end
   end
 
   def destroy
-    @planner = Planner.find(params[:id])
-    @planner.destroy
-    redirect_to @planner
+    @shopping_list.destroy
+    redirect_to shopping_lists_path
   end
 
   private
 
-  def shopping_list__params
+  def shopping_list_params
     params.require(:shopping_list).permit(:title)
   end
 
