9fe894767086286e7b6a754321b05164ee707388
Shari Breitenstein-Werner <shari_breitenstein@yahoo.de>
2021-05-25 19:13:44 +0800

    added ingredients form and utensils form

--- app/controllers/recipe_utensils_controller.rb
+++ app/controllers/recipe_utensils_controller.rb
@@ -1,7 +1,40 @@
 class RecipeUtensilsController < ApplicationController
+    before_action :set_recipe_utensil, only: [:show, :edit, :update, :destroy]
+    before_action :set_recipe, only: [:index, :create]
+
+    def index
+        @recipe_utensils = @recipe.recipe_utensils
+        @recipe_utensil = RecipeUtensil.new
+    end
+    
+    def create
+
+    end
+    
+    def edit
+    end 
+    
+    def update
+    end
+
+
     def destroy
         @recipe_utensil = RecipeUtensil.find(params[:id])
         @recipe_utensil.destroy
         redirect_to @recipe_utensil.recipe
+    end
+
+    private
+
+    def set_recipe
+        @recipe = Recipe.find(params[:recipe_id]) 
+    end
+
+    def set_recipe_utensil
+        @recipe_utensil = RecipeUtensil.find(params[:id])
+    end
+
+    def recipe_utensil_params
+        params.require(:recipe_utensil).permit(:quantity)
     end
 end
