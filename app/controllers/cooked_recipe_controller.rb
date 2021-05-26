0ab9c6b65d731312fd1153ddc9d491b905a0a0aa
Jacqueline <j.sutjiawan@gmail.com>
2021-05-26 16:46:04 +0800

    still working on mark recipe

---
+++ app/controllers/cooked_recipe_controller.rb
@@ -0,0 +1,17 @@
+class CookedRecipeController < ApplicationController
+  def create
+    @cooked_recipe = CookedRecipe.new
+    @recipe = Recipe.find(params[:recipe_id])
+    @cooked_recipe.recipe = @recipe
+    @cooked_recipe.user = current_user
+    @cooked_recipe.save
+    redirect_to request.referer
+  end
+
+  def destroy
+    @cooked_recipe = CookedRecipe.find(params[:id])
+    @cooked_recipe.destroy
+    redirect_to request.referer
+  end
+
+end
---
+++ test/controllers/cooked_recipe_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class CookedRecipeControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
