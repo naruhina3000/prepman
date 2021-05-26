3e74b44d1da97566a25f8b6bcac3ad12e587512b
Jacqueline <j.sutjiawan@gmail.com>
2021-05-26 15:16:07 +0800

    added status to recipe & added create new shopping list when adding ingredients to shopping list

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -2,13 +2,13 @@ class Recipe < ApplicationRecord
   attr_accessor :associated_list
   has_one_attached :photo
   belongs_to :user
-  has_many :planners
-  has_many :steps
-  has_many :recipe_ingredients
+  has_many :planners, dependent: :destroy
+  has_many :steps, dependent: :destroy
+  has_many :recipe_ingredients, dependent: :destroy
   has_many :ingredients, through: :recipe_ingredients
-  has_many :recipe_utensils
+  has_many :recipe_utensils, dependent: :destroy
   has_many :utensils, through: :recipe_utensils
-  has_many :shopping_list_ingredients
+  has_many :shopping_list_ingredients, dependent: :destroy
   has_many :shopping_lists, through: :shopping_list_ingredients
   has_many :cookbook_recipes
   has_many :cookbooks, through: :cookbook_recipes
@@ -21,7 +21,7 @@ class Recipe < ApplicationRecord
   DISHTYPE = ["Starter", "Main", "Dessert", "Beverages", "Snacks", "Breakfast", "Side Dish"]
   CUISINE = ["Chinese", "Italian", "European", "American", "Iberian", "Indian", "Middle Eastern", "Japanese", "Korean", "Mexican", "Indonesian", "Others"]
   OCCASION = ["All Day Recipes", "Weeknight", "Prepare ahead", "Crowd Pleaser", "On the go", "Comfort Food", "Kid-friendly", "Finger food", "BBQ", "Christmas", "Easter", "Valentines Day", "Thanksgiving", "Halloween", "October Fest"]
-
+  STATUS = ["draft", "published"]
   validates :title, presence: true
   validates :description, presence: true
   validates :difficulty, inclusion: { in: DIFFICULTY }
@@ -29,6 +29,7 @@ class Recipe < ApplicationRecord
   validates :dish_type, inclusion: { in: DISHTYPE }
   validates :cuisine, inclusion: { in: CUISINE }
   validates :occasion, inclusion: { in: OCCASION }
+  validates :status, inclusion: { in: STATUS }
 
   # validate :total_time
 
--- app/models/shopping_list_ingredient.rb
+++ app/models/shopping_list_ingredient.rb
@@ -3,7 +3,10 @@ class ShoppingListIngredient < ApplicationRecord
   belongs_to :recipe, optional: true
   belongs_to :shopping_list
 
+  UNIT = ["ml", "gr", "unit"]
+
 
   validates :quantity, presence: true
   validates :shopping_list, uniqueness: { scope: :ingredient }
+  validates :unit, inclusion: { in: UNIT }
 end
