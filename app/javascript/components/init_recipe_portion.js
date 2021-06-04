7f43aa46776b01afda56179d3a01d42cbd146ff1
Shari Breitenstein-Werner <shari_breitenstein@yahoo.de>
2021-06-04 16:32:47 +0800

    oh nooooooo2

--- app/javascript/components/init_recipe_portion.js
+++ app/javascript/components/init_recipe_portion.js
@@ -1,30 +1,28 @@
 const updateIngredientsAmount = (portion) => {
-  const amounts = document.querySelectorAll('.ingredient-amount')
-  document.querySelector('#recipe_portion').value = portion
+  const amounts = document.querySelectorAll(".ingredient-amount");
+  document.querySelector("#recipe_portion").value = portion;
   amounts.forEach((amount) => {
-    amount.innerText = parseInt(amount.dataset.unit) * portion
-  })
-}
+    amount.innerText = parseFloat(amount.dataset.unit) * portion;
+  });
+};
 
 const initRecipePortion = () => {
-  const amount = document.querySelector('#portion-amount')
-  if (amount){
-  const less =  amount.previousElementSibling
-  const more =  amount.nextElementSibling
-    less.addEventListener('click', ()=>{
-      if (amount.innerText === '1') return
-      const portion = parseInt(amount.innerText) - 1
-      amount.innerText = portion
-      updateIngredientsAmount(portion)
-    })
-    more.addEventListener('click', ()=>{
-      const portion = parseInt(amount.innerText) + 1
-      amount.innerText = portion
-      updateIngredientsAmount(portion)
-
-    })
+  const amount = document.querySelector("#portion-amount");
+  if (amount) {
+    const less = amount.previousElementSibling;
+    const more = amount.nextElementSibling;
+    less.addEventListener("click", () => {
+      if (amount.innerText === "1") return;
+      const portion = parseFloat(amount.innerText) - 1;
+      amount.innerText = portion;
+      updateIngredientsAmount(portion);
+    });
+    more.addEventListener("click", () => {
+      const portion = parseFloat(amount.innerText) + 1;
+      amount.innerText = portion;
+      updateIngredientsAmount(portion);
+    });
   }
-}
+};
 
-
-export { initRecipePortion }
+export { initRecipePortion };
